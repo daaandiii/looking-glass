32188a52e4fca02fb213fbc0acc8dda70b1a0525
th3beekeeper <muhamadriff253104@gmail.com>
2025-07-25 10:46:42 +0700

    update command deskripsi

--- includes/config.defaults.php
+++ includes/config.defaults.php
@@ -204,31 +204,31 @@ $config = array(
   'doc' => array(
     // Documentation for the 'show route' query
     'bgp' => array(
-      'command' => 'show route IP_ADDRESS',
+      'command' => 'show route',
       'description' => 'Show the best routes to a given destination.',
       'parameter' => 'The parameter must be a valid destination. Destination means an IPv4/IPv6 address or a subnet. Masks are also accepted as part of a valid IPv4/IPv6 address.<br />RFC1918 addresses, IPv6 starting with FD or FC, and IPv4 reserved ranges (0.0.0.0/8, 169.254.0.0/16, 192.0.2.0/24 and 224.0.0.0/4) may be refused.<br />Please note that some routers always need a mask to be given when looking for an IPv6 address.<br /><br />Example of valid arguments:<br /><ul><li>8.8.8.8</li><li>8.8.4.0/24</li><li>2001:db8:1337::42</li><li>2001:db8::/32</li>'
     ),
     // Documentation for the 'as-path-regex' query
     'as-path-regex' => array(
-      'command' => 'show route as-path-regex AS_PATH_REGEX',
+      'command' => 'show route as-path-regex',
       'description' => 'Show the routes matching the given AS path regular expression.',
       'parameter' => 'The parameter must be a valid AS path regular expression and must not contain any " characters (the input will be automatically quoted if needed).<br />Please note that these expressions can change depending on the router and its software.<br />OpenBGPD does not support regular expressions, but will search for the submitted AS number anywhere in the AS path.<br /><br />Here are some examples:<ul><li><strong>Juniper</strong> - ^AS1 AS2 .*$</li><li><strong>Cisco</strong> - ^AS1_AS2_</li><li><strong>BIRD</strong> - AS1 AS2 AS3 &hellip; ASZ</li><li><strong>OpenBGPD</strong> - AS1</li></ul><br />You may find some help with the following link:<br /><ul><li><a href="http://www.juniper.net/techpubs/en_US/junos13.3/topics/reference/command-summary/show-route-aspath-regex.html" title="Juniper Documentation">Juniper Documentation</a></li><li><a href="http://www.cisco.com/c/en/us/support/docs/ip/border-gateway-protocol-bgp/26634-bgp-toc.html#asregexp" title="Cisco Documentation">Cisco Documentation</a></li><li><a href="http://bird.network.cz/?get_doc&f=bird-5.html" title="BIRD Documentation">BIRD Documentation</a> (search for bgpmask)</li></ul>'
     ),
     // Documentation for the 'as' query
     'as' => array(
-      'command' => 'show route ^AS',
+      'command' => 'show route AS Number',
       'description' => 'Show the routes received from a given neighboring AS number.',
       'parameter' => 'The parameter must be a valid 16-bit or 32-bit autonomous system number.<br />Be careful, 32-bit ASN are not handled by old routers or old router softwares.<br />Unless specified, private ASN will be considered as invalid.<br /><br />Example of valid argument:<br /><ul><li>15169</li><li>29467</li></ul>'
     ),
     // Documentation for the 'ping' query
     'ping' => array(
-      'command' => 'ping IP_ADDRESS|HOSTNAME',
+      'command' => 'ping',
       'description' => 'Send pings to the given destination.',
       'parameter' => 'The parameter must be an IPv4/IPv6 address (without mask) or a hostname.<br />RFC1918 addresses, IPv6 starting with FD or FC, and IPv4 reserved ranges (0.0.0.0/8, 169.254.0.0/16, 192.0.2.0/24 and 224.0.0.0/4) may be refused.<br /><br />Example of valid arguments:<br /><ul><li>8.8.8.8</li><li>2001:db8:1337::42</li><li>example.com</li></ul>'
     ),
     // Documentation for the 'traceroute' query
     'traceroute' => array(
-      'command' => 'traceroute IP_ADDRESS|HOSTNAME',
+      'command' => 'traceroute',
       'description' => 'Display the path to a given destination.',
       'parameter' => 'The parameter must be an IPv4/IPv6 address (without mask) or a hostname.<br />RFC1918 addresses, IPv6 starting with FD or FC, and IPv4 reserved ranges (0.0.0.0/8, 169.254.0.0/16, 192.0.2.0/24 and 224.0.0.0/4) may be refused.<br /><br />Example of valid arguments:<br /><ul><li>8.8.8.8</li><li>2001:db8:1337::42</li><li>example.com</li></ul>'
     )
